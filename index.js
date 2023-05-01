516685f0dc00cb14801b72779a056568404f9d6d
chikki-admin <chikkiaquatics@gmail.com>
2023-05-01 15:02:12 -0500

    added more CORS headers

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(cors())
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,POST,GET');
   next();
 });
 
