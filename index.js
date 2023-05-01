5987d5c02e50ba604b83f72044abe254c9a5df2a
chikki-admin <chikkiaquatics@gmail.com>
2023-05-01 00:00:02 -0500

    setup cors

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ app.use(bodyParser.json({
 }))
 
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Local constants
 const shippingCost = 52
